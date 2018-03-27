a386d6651c3584ce77ae401b5eb9f577827d5562
Neeraj <neeraj.moudgil@smartdatainc.net>
2018-03-27 17:35:34 +0530

    First screen burns effect

--- app/src/main/java/com/neerajm/roadeazy/MainActivity.java
+++ app/src/main/java/com/neerajm/roadeazy/MainActivity.java
@@ -2,6 +2,7 @@ package com.neerajm.roadeazy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideStatusBar();
+    }
+
+    private void hideStatusBar() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
